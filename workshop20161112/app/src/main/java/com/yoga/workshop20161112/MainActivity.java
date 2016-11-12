0004c239f653ce9479e851e20ed94b4d42460771
YOGA <art96962006@gmail.com>
2016-11-12 12:44:42 +0800

    Initialize the android workshop project.

---
+++ workshop20161112/app/src/main/java/com/yoga/workshop20161112/MainActivity.java
@@ -0,0 +1,13 @@
+package com.yoga.workshop20161112;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
